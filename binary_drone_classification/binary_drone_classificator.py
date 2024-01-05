9bae133e665b3f35e697c670c658c58221b30f9e
Simon <simonmyhre1@gmail.com>
2024-01-05 19:02:48 +0100

    Add binary drone classificator class

---
+++ binary_drone_classification/binary_drone_classificator.py
@@ -0,0 +1,11 @@
+
+class BinaryDroneClassificator:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("BinaryDroneClassificator")
+
+if __name__ == "__main__":
+    binary_drone_classificator = BinaryDroneClassificator()
+    binary_drone_classificator.run()
