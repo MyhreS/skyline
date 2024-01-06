932185bf3cee69cfdfd6d361e7a8b16e40dbcf79
Simon <simonmyhre1@gmail.com>
2024-01-06 14:19:16 +0100

    Import paths

--- binary_drone_classification/binary_drone_classificator.py
+++ binary_drone_classification/binary_drone_classificator.py
@@ -1,10 +1,18 @@
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 class BinaryDroneClassificator:
     def __init__(self):
-        pass
+        self.data_path = os.getenv("BINARY_DRONE_DATA_PATH")
 
     def run(self):
         print("BinaryDroneClassificator")
+        print("Data path: {}".format(self.data_path))
+        print(os.listdir(self.data_path))
+
 
 if __name__ == "__main__":
     binary_drone_classificator = BinaryDroneClassificator()
