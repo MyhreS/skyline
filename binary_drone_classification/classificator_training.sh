cb0184f50e9c9dbfa548bd597290ac1e095ee60f
Simon Myhre <simon@sqhead.com>
2024-01-21 14:56:11 +0100

    Change node to hpc2

--- binary_drone_classification/classificator_training.sh
+++ binary_drone_classification/classificator_training.sh
@@ -2,13 +2,13 @@
 #SBATCH -A master # Replace with your account name
 #SBATCH -p normal # Replace with the desired partition name
 #SBATCH --gres=gpu:1 # Request 1 GPU
-#SBATCH --nodelist=hpc2 # Use node hpc2
+#SBATCH --nodelist=hpc2
 #SBATCH --output=cache/classificator_training.log
 #SBATCH --error=cache/classificator_training.log
 #SBATCH --open-mode=truncate
 
 # Load modules
-module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
+module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0:
 
 # Load any necessary modules or activate a virtual environment
 source ~/skyline/skylineenv/bin/activate
