e02d6a86741f52746e05264047fad8e9b282bb8b
Simon Myhre <simon@sqhead.com>
2024-03-10 16:08:42 +0100

    Add working training of a model

--- binary_drone_classification/simple_model/simple_model.py
+++ binary_drone_classification/simple_model/simple_model.py
@@ -1,9 +1,9 @@
 PATH_TO_SKYLINE = "/cluster/datastore/simonmy/skyline"  # "/workspace/skyline"
-PATH_TO_INPUT_DATA = "/cluster/datastore/simonmy/data/datav2"  # "/workspace/data/data"
+PATH_TO_INPUT_DATA = "/cluster/datastore/simonmy/data/datav1"  # "/workspace/data/data"
 PATH_TO_OUTPUT_DATA = (
     "/cluster/datastore/simonmy/skyline/cache/data"  # "/workspace/skyline/cache/data"
 )
-RUN_NAME = "run_2"
+RUN_NAME = "run_3"
 import sys
 
 sys.path.append(PATH_TO_SKYLINE)
@@ -37,13 +37,19 @@ data.set_label_class_map(
             "petrol_fixedwing",
             "racing_drone",
         ],
-        "non-drone": ["nature_chernobyl", "false_positives_drone", "speech"],
+        "non-drone": ["nature_chernobyl", "false_positives_drone"],
     }
 )
+data.remove_label("false_positives_drone")
+data.remove_label("normal_fixedwing")
+data.remove_label("petrol_fixedwing")
+data.remove_label("racing_drone")
+# data.remove_label("speech")
+
 data.set_sample_rate(44100)
-data.set_augmentations(
-    ["low_pass", "pitch_shift", "add_noise", "high_pass", "band_pass"]
-)
+# data.set_augmentations(
+#     ["low_pass", "pitch_shift", "add_noise", "high_pass", "band_pass"]
+# )
 data.set_audio_format("log_mel")
 data.set_file_type("tfrecord")
 data.set_limit(50_000)
@@ -101,11 +107,9 @@ model = tf.keras.Sequential(
         layers.Flatten(),
         layers.Dense(256),
         layers.LeakyReLU(alpha=0.01),
-        layers.BatchNormalization(),
         layers.Dropout(0.5),
         layers.Dense(128),
         layers.LeakyReLU(alpha=0.01),
-        layers.BatchNormalization(),
         layers.Dense(1, activation="sigmoid"),
     ]
 )
@@ -121,16 +125,15 @@ model.compile(
 )
 
 callbacks = []
-callbacks.append(EarlyStopping(monitor="val_loss", patience=10))
+callbacks.append(EarlyStopping(monitor="val_loss", patience=4))
 callbacks.append(TensorBoard(log_dir=logger.get_tensorboard_path(), histogram_freq=1))
 
 
 history = model.fit(
     train_ds,
     validation_data=val_ds,
-    epochs=30,
+    epochs=7,
     callbacks=callbacks,
-    class_weight=class_weights,
 )
 logger.save_model(model)
 logger.save_model_train_history(history.history)
@@ -154,10 +157,10 @@ test_racing_drone_ds, shape = data.load_it(
 test_nature_chernobyl_ds, shape = data.load_it(
     split="test_nature_chernobyl", label_encoding="integer"
 )
-test_false_positive_ds, shape = data.load_it(
-    split="test_false_positives_drone", label_encoding="integer"
-)
-test_speech_ds, shape = data.load_it(split="test_speech", label_encoding="integer")
+# test_false_positive_ds, shape = data.load_it(
+#     split="test_false_positives_drone", label_encoding="integer"
+# )
+# test_speech_ds, shape = data.load_it(split="test_speech", label_encoding="integer")
 
 
 # Test evaluations
@@ -177,24 +180,24 @@ logging.info("Nature chernobyl test ds")
 nature_chernobyl_test_loss, nature_chernobyl_test_acc = model.evaluate(
     test_nature_chernobyl_ds
 )
-logging.info("False positive test ds")
-false_positive_test_loss, false_positive_test_acc = model.evaluate(
-    test_false_positive_ds
-)
-logging.info("Speech test ds")
-speech_test_loss, speech_test_acc = model.evaluate(test_speech_ds)
+# logging.info("False positive test ds")
+# false_positive_test_loss, false_positive_test_acc = model.evaluate(
+#     test_false_positive_ds
+# )
+# logging.info("Speech test ds")
+# speech_test_loss, speech_test_acc = model.evaluate(test_speech_ds)
 
-test_results = {
-    "normal_drone": normal_drone_test_acc,
-    "normal_fixedwing": normal_fixedwing_test_acc,
-    "petrol_fixedwing": petrol_fixedwing_test_acc,
-    "racing_drone": racing_drone_test_acc,
-    "nature_chernobyl": nature_chernobyl_test_acc,
-    "false_positive": false_positive_test_acc,
-    "speech": speech_test_acc,
-}
-logger.save_model_test_results(test_results)
+# test_results = {
+#     "normal_drone": normal_drone_test_acc,
+#     "normal_fixedwing": normal_fixedwing_test_acc,
+#     "petrol_fixedwing": petrol_fixedwing_test_acc,
+#     "racing_drone": racing_drone_test_acc,
+#     "nature_chernobyl": nature_chernobyl_test_acc,
+#     "false_positive": false_positive_test_acc,
+#     "speech": speech_test_acc,
+# }
+# logger.save_model_test_results(test_results)
 
-# Get the average accuracy
-average_accuracy = sum(test_results.values()) / len(test_results)
-print(f"Average accuracy: {average_accuracy}")
+# # Get the average accuracy
+# average_accuracy = sum(test_results.values()) / len(test_results)
+# print(f"Average accuracy: {average_accuracy}")
